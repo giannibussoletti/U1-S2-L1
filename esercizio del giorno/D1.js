40cf95d7fe4e02986594b0cb79fd8e2effb22c94
Gianni <BussolettiGianni@gmail.com>
2026-03-09 15:49:44 +0100

    Update D1.js

--- esercizio del giorno/D1.js
+++ esercizio del giorno/D1.js
@@ -15,20 +15,20 @@ REGOLE
 
 //  number -> tutti i numeri dichiarati senza le virgolette fanno parte del datatype number. Questo DT può comprendere numeri di ogni tipo, interi come 1, 4, 100. decimali come 13.49, 100.05. Anche i numeri negativi fanno parte del gruppo e possono essere sia interi che decimali: -10 -200 -20.4 -35.004
 
-/* - string -> "stefano" "gianni" "latte&miele" (stringa vuota)"" o anche con il numero al suo interno "doggo10"
- delimitati con '' - "" - `` <--  quest'ultimo anche è un delimitatore e lo si può richiamare con alt+96 */
-let stringaVar = "dichiarazione di stringa"
+// - string -> "stefano" "gianni" "latte&miele" "" sono tutti datatype di tipo stringa, la cosa fondamentale è che siano chiusi tra "", '' oppure ``. perché altrimenti verrebbero lette come il nome di una variabile. All'interno delle virgolette può andare qualsiasi tipo di carattere compresi i numeri, in questo caso vengono riconosciuti semplicemente come simboli di una stringa.
+// - boolean -> può dare solo due tipi di risultato: TRUE o FALSE. Questi risultati sono la risposta ad un tipo di comparazione fra due o più variabili. Queste variabili vengono calcolate con i seguenti simboli chiamati OPERATORI DI COMPARAZIONE:
+// < Controlla se una variabile è minore di un'altra
+// > Controlla se una variabile è maggiore di un'altra
+// <= Controlla se una variabile è minore o uguale ad un altra
+// ! Il punto esclamativo inverte il risultato di una variabile facendolo passare da TRUE a FALSE e viceversa, viene messo prima del nome della variabile. Es: !variabile
+// === I tre Uguali controllano che tue variabili abbiano lo stesso Valore e siano dello stesso TIPO, nel caso di una variabile number e una stringa il risultato sarà FALSE per esempio.
+// !== Il punto esclamativo invece fa il contrario dei 3 uguali. Controlla se una variabile è differente dall'altra, come nell'esempio di prima una variabile number è diversa da una stringa e quindi il suo risultato sarà TRUE.
 
-// - boolean -> true, false
-let varBooleana = 10 < 5 // il risultato sarà false
-varBooleana = true // Sia true che false sono proprio dichiarazioni non stringhe
-varBooleana = false
+// - undefined -> è il valore di default di una variabile a cui non è stato assegnato alcun valore.
+let varUn // Il valore della variabile non è stato dichiarato e quindi la variabile è considerata UNDEFINED.
 
-// - undefined -> valore non definito
-let varUn // Il valore della variabile non è stato dichiarato
-// diventa automaticamente undefined
-
-// - null ->
+// - null -> Dichiara invece che la variabile è vuota e non restituisce alcun risultato c'è una ASSENZA ESPLICITA di valore.
+let varVuota = null
 
 /* ESERCIZIO 2
  Crea una variable chiamata "myName" e assegna ad essa il tuo nome, sotto forma di stringa.
@@ -101,3 +101,10 @@ console.log(sameName)
 /* Un altro modo per mettere a confronto le due stringhe è usando !== che va a dimostrare come queste due siano effettivamente diverse dando il risultato TRUE */
 let diffName = name1 !== name2
 console.log(diffName)
+
+// Per dimostrare invece che le due stringhe sarebbero identiche se fossero entrambi lowercase ho trovato questa soluzione
+
+sameName2 = name1 === name2.toLowerCase()
+console.log(sameName2)
+
+//In questo caso il comando .toLowerCase() rende temporaneamente John con la maiuscola, una parola tutta in minuscolo. Andando a confermare che le variabili sarebbero identiche se tutte e due fossero minuscole.
