fe98ffd284d8b66846974fcd8067cd7da9bfeef7
Gianni <BussolettiGianni@gmail.com>
2026-03-09 14:56:48 +0100

    Update D1.js

--- esercizio del giorno/D1.js
+++ esercizio del giorno/D1.js
@@ -19,11 +19,19 @@ REGOLE
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+let myName = "Gianni"
+console.log(myName)
+
 /* ESERCIZIO 3
  Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+let a = 12
+let b = 20
+
+let c = a + b
+console.log(c)
 
 /* ESERCIZIO 4
  Crea una variable di nome "x" e assegna ad essa il numero 12.
@@ -31,18 +39,26 @@ REGOLE
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
+let x = 12
+
 /* ESERCIZIO 5
   Riassegna un nuovo valore alla variabile "myName" già esistente: il tuo cognome.
   Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+myName = "Bussoletti"
+console.log(myName)
+
+const varCostante = 25
+varCostante = 20 /* Provando a riassegnare un nuovo valore a questa variabile, nella console del broswer viene fuori un messaggio di errore che notifica che la variabile non può essere riassegnata "Uncaught TypeError: invalid assignment to const 'varCostante'" */
 
 /* ESERCIZIO 6
  Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+console.log(x - 4)
 
 /* ESERCIZIO 7
  Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
